5736b8322330be068b86587e4a7c6f7696beff5e
Saikat Karmakar <saikatforever@hotmail.com>
2026-04-03 00:32:28 +0530

    Add Core/Constants.cs for entity roles and actions

---
+++ Core/Constants.cs
@@ -0,0 +1,8 @@
+// Constants for entities, roles, and custom actions
+
+public static class Constants
+{
+    public const string EntityRoleAdmin = "Admin";
+    public const string EntityRoleUser = "User";
+    // Add more constants here
+}
